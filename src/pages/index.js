1f101ba224fe3c08e6017739005f55fffd3a7aca
Glenn-Erik Sandbakken <glenn.dev.erik@gmail.com>
2021-09-28 18:03:59 +0200

    try add canonical

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import * as React from "react"
 import { Heading, Text } from "@chakra-ui/react"
+import { Helmet } from "react-helmet"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Arndp from "../components/Arndp"
 
 const IndexPage = () => (
   <Layout>
+    <Helmet>
+      <link rel="canonical" href="https://www.arndp.com/" />
+    </Helmet>
     <Seo title="A Random Password Generator" />
     <Heading as="h1">A Random Password Generator</Heading>
     <Text as="p">Click on a password to copy it</Text>
