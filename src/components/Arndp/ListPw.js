b7fdb9bfe23a36eeb61b118fdcdcc4de34e37c1d
Glenn-Erik Sandbakken <glenn.dev.erik@gmail.com>
2021-09-28 12:39:34 +0200

    add react-copy-to-clipboard

--- src/components/Arndp/ListPw.js
+++ src/components/Arndp/ListPw.js
@@ -1,16 +1,19 @@
 import * as React from "react"
+import { CopyToClipboard } from "react-copy-to-clipboard"
 
 export const ListPw = ({ password }) => (
-  <pre style={{ textAlign: "center", cursor: "copy" }}>
-    <span
-      style={{
-        background: "lightcyan",
-        border: "1px dotted gray",
-        color: "gray",
-        fontSize: "1.1rem",
-      }}
-    >
-      {password}
-    </span>
-  </pre>
+  <CopyToClipboard text={password} onCopy={() => console.log("copied")}>
+    <pre style={{ textAlign: "center", cursor: "copy" }}>
+      <span
+        style={{
+          background: "lightcyan",
+          border: "1px dotted gray",
+          color: "gray",
+          fontSize: "1.1rem",
+        }}
+      >
+        {password}
+      </span>
+    </pre>
+  </CopyToClipboard>
 )
