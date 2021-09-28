1afea4748414e8209c9ceae75e54f6e2b09fc57b
Glenn-Erik Sandbakken <glenn.dev.erik@gmail.com>
2021-09-28 12:47:40 +0200

    use sweetalert2

--- src/components/Arndp/ListPw.js
+++ src/components/Arndp/ListPw.js
@@ -1,8 +1,20 @@
 import * as React from "react"
 import { CopyToClipboard } from "react-copy-to-clipboard"
+import Swal from "sweetalert2"
 
 export const ListPw = ({ password }) => (
-  <CopyToClipboard text={password} onCopy={() => console.log("copied")}>
+  <CopyToClipboard
+    text={password}
+    onCopy={() =>
+      Swal.fire({
+        title: "Password Copied",
+        text: "hit ctrl+v or cmd+v to save it somewhere",
+        icon: "success",
+        showCloseButton: true,
+        confirmButtonText: "Cool",
+      })
+    }
+  >
     <pre style={{ textAlign: "center", cursor: "copy" }}>
       <span
         style={{
