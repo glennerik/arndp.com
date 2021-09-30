a79caed47ee7c02d4f2ebde9d726e42bc3dcff5c
Glenn-Erik Sandbakken <glenn.dev.erik@gmail.com>
2021-09-30 10:53:28 +0200

    improved listPw box style

--- src/components/Arndp/ListPw.js
+++ src/components/Arndp/ListPw.js
@@ -1,31 +1,52 @@
 import * as React from "react"
+import { useState, useEffect } from "react"
 import { CopyToClipboard } from "react-copy-to-clipboard"
 import Swal from "sweetalert2"
+import { ScaleFade, Text } from "@chakra-ui/react"
 
-export const ListPw = ({ password }) => (
-  <CopyToClipboard
-    text={password}
-    onCopy={() =>
-      Swal.fire({
-        title: "Password Copied",
-        text: "hit ctrl+v or cmd+v to save it somewhere",
-        icon: "success",
-        showCloseButton: true,
-        confirmButtonText: "Cool",
-      })
-    }
-  >
-    <pre style={{ textAlign: "center", cursor: "copy" }}>
-      <span
-        style={{
-          background: "lightcyan",
-          border: "1px dotted gray",
-          color: "gray",
-          fontSize: "1.1rem",
-        }}
+export const ListPw = ({ password }) => {
+  const [isOpen, setOpen] = useState(false)
+
+  useEffect(() => {
+    setOpen(true)
+    return () => setOpen(false)
+  }, [])
+
+  return (
+    <ScaleFade in={isOpen}>
+      <CopyToClipboard
+        text={password}
+        onCopy={() =>
+          Swal.fire({
+            title: "Password Copied",
+            text: "hit ctrl+v or cmd+v to save it somewhere",
+            icon: "success",
+            showCloseButton: true,
+            confirmButtonText: "Cool",
+          })
+        }
       >
-        {password}
-      </span>
-    </pre>
-  </CopyToClipboard>
-)
+        <Text
+          as="pre"
+          bg="purple.400"
+          rounded="md"
+          shadow="md"
+          cursor="copy"
+          my={4}
+          textAlign="center"
+        >
+          <Text
+            as="span"
+            borderWidth={1}
+            borderStyle="dotted"
+            borderColor="white"
+            fontSize="xl"
+            color="white"
+          >
+            {password}
+          </Text>
+        </Text>
+      </CopyToClipboard>
+    </ScaleFade>
+  )
+}
