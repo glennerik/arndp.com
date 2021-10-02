a72df63f58b4fc4bb79ddc0f79805184b69ce4fe
Glenn-Erik Sandbakken <glenn.dev.erik@gmail.com>
2021-10-02 15:31:45 +0200

    add googletagmanager

--- src/html.js
+++ src/html.js
@@ -11,6 +11,21 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZZWM5YKPRK"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'G-ZZWM5YKPRK');
+        
+        `,
+          }}
+        />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
