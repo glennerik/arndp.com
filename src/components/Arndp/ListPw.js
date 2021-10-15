106c14648e4badffd3520ae50c9048f847c3649a
Glenn-Erik Sandbakken <glenn.dev.erik@gmail.com>
2021-10-15 13:33:50 +0200

    remove useless useState

--- src/components/Arndp/ListPw.js
+++ src/components/Arndp/ListPw.js
@@ -1,21 +1,14 @@
 import * as React from "react"
-import { useState, useEffect } from "react"
 import { CopyToClipboard } from "react-copy-to-clipboard"
 import Swal from "sweetalert2"
 import { ScaleFade, Text } from "@chakra-ui/react"
 import { useGetThemeColor } from "../ThemeProvider"
 
 export const ListPw = ({ password, hide }) => {
-  const [isOpen, setOpen] = useState(false)
   const { themeColor } = useGetThemeColor()
 
-  useEffect(() => {
-    setOpen(true)
-    return () => setOpen(false)
-  }, [])
-
   return (
-    <ScaleFade in={isOpen}>
+    <ScaleFade in={true}>
       <CopyToClipboard
         text={password}
         onCopy={() =>
