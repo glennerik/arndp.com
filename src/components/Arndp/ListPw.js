2a7a4447e437819d8c1ef0e8e09cdf064dd0473b
Glenn-Erik Sandbakken <glenn.dev.erik@gmail.com>
2021-10-18 09:10:15 +0200

    add tooltip "click to copy" on passwords

--- src/components/Arndp/ListPw.js
+++ src/components/Arndp/ListPw.js
@@ -1,52 +1,57 @@
 import * as React from "react"
 import { CopyToClipboard } from "react-copy-to-clipboard"
 import Swal from "sweetalert2"
-import { ScaleFade, Text, Box } from "@chakra-ui/react"
+import { ScaleFade, Text, Box, Tooltip } from "@chakra-ui/react"
+import { isMobile } from "react-device-detect"
 import { useGetThemeColor } from "../ThemeProvider"
+
+const Tt = p => (isMobile ? p.children : <Tooltip {...p} />)
 
 export const ListPw = ({ password, hide }) => {
   const { themeColor } = useGetThemeColor()
 
   return (
-    <ScaleFade in={true}>
-      <CopyToClipboard
-        text={password}
-        onCopy={() =>
-          Swal.fire({
-            title: "Password Copied",
-            text: "hit ctrl+v or cmd+v to save it somewhere",
-            icon: "success",
-            showCloseButton: true,
-            confirmButtonText: "Cool",
-            confirmButtonColor: themeColor,
-          })
-        }
-      >
-        <Box
-          bg={themeColor}
-          rounded="md"
-          shadow="md"
-          cursor="copy"
-          textAlign="center"
-          my={4}
-          p={4}
+    <Tt label="Click to copy" hasArrow>
+      <ScaleFade in={true}>
+        <CopyToClipboard
+          text={password}
+          onCopy={() =>
+            Swal.fire({
+              title: "Password Copied",
+              text: "hit ctrl+v or cmd+v to save it somewhere",
+              icon: "success",
+              showCloseButton: true,
+              confirmButtonText: "Cool",
+              confirmButtonColor: themeColor,
+            })
+          }
         >
-          <Text
-            as="pre"
-            wordBreak="break-all"
-            whiteSpace="pre-wrap"
-            fontFamily="mono"
-            d="inline"
-            borderWidth={1}
-            borderStyle="dotted"
-            borderColor="white"
-            fontSize="xl"
-            color="white"
+          <Box
+            bg={themeColor}
+            rounded="md"
+            shadow="md"
+            cursor="copy"
+            textAlign="center"
+            my={4}
+            p={4}
           >
-            {hide ? "****************" : password}
-          </Text>
-        </Box>
-      </CopyToClipboard>
-    </ScaleFade>
+            <Text
+              as="pre"
+              wordBreak="break-all"
+              whiteSpace="pre-wrap"
+              fontFamily="mono"
+              d="inline"
+              borderWidth={1}
+              borderStyle="dotted"
+              borderColor="white"
+              fontSize="xl"
+              color="white"
+            >
+              {hide ? "****************" : password}
+            </Text>
+          </Box>
+        </CopyToClipboard>
+      </ScaleFade>
+    </Tt>
   )
 }
